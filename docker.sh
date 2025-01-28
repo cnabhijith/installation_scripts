f9f3fc8c1d2aeb7133292654a1e7e3174dc89153
Abhijith_C_N <abhijithcn28@gmail.com>
2025-01-28 17:37:28 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,18 +1,21 @@
-#!/bin/bash
-sudo apt update -y
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+------
 
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+--
+sudo usermod -aG docker $USER
 
-sudo apt update -y
-
-apt-cache policy docker-ce -y
-
-sudo apt install docker-ce -y
-
-#sudo systemctl status docker
-
-sudo chmod 777 /var/run/docker.sock
+newgrp docker
