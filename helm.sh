ccc731877f07c6df0c929d70502206ae3e6ff8b4
Abhijith_C_N <abhijithcn28@gmail.com>
2025-01-28 14:11:20 +0530

    Update helm.sh

--- helm.sh
+++ helm.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
+#Install HELM
+sudo apt update && sudo apt upgrade -y
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
-echo "Install Helm Chart Using Script"
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
-chmod 700 get_helm.sh
-./get_helm.sh
-
-echo "checking version.."
+wget https://get.helm.sh/helm-v3.14.0-linux-amd64.tar.gz
+tar -zxvf helm-v3.14.0-linux-amd64.tar.gz
+sudo mv linux-amd64/helm /usr/local/bin/helm
 helm version
